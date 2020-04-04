49a4b6dc83d4b7ea73324f05685b1040672a06de
Przemyslaw Pajak <przemyslaw.pajak@creative-works.us>
2020-04-04 17:40:22 +0200

    Add popover

--- CreativeWorksTimer/CreativeWorksTimer/AppDelegate.swift
+++ CreativeWorksTimer/CreativeWorksTimer/AppDelegate.swift
@@ -12,8 +12,8 @@ import SwiftUI
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-	var window: NSWindow!
-
+	var popover = NSPopover.init()
+	let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
@@ -21,14 +21,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		let contentView = ContentView().environment(\.managedObjectContext, persistentContainer.viewContext)
 
 		// Create the window and set the content view. 
-		window = NSWindow(
-		    contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-		    styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-		    backing: .buffered, defer: false)
-		window.center()
-		window.setFrameAutosaveName("Main Window")
-		window.contentView = NSHostingView(rootView: contentView)
-		window.makeKeyAndOrderFront(nil)
+		popover.contentSize = NSSize(width: 360, height: 360)
+		popover.contentViewController = MainViewController()
+		popover.contentViewController?.view = NSHostingView(rootView: contentView)
+
+		if let button = statusItem.button {
+			button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+			button.action = #selector(togglePopover(_:))
+		}
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
@@ -131,6 +131,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	    // If we got here, it is time to quit.
 	    return .terminateNow
 	}
+	
+	@objc func togglePopover(_ sender: Any?) {
+	  if popover.isShown {
+		closePopover(sender: sender)
+	  } else {
+		showPopover(sender: sender)
+	  }
+	}
 
+	func showPopover(sender: Any?) {
+	  if let button = statusItem.button {
+		popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+	  }
+	}
+
+	func closePopover(sender: Any?) {
+	  popover.performClose(sender)
+	}
 }
 
---
+++ CreativeWorksTimer/CreativeWorksTimer/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  CreativeWorksTimer
+//
+//  Created by Przemyslaw Pajak on 04/04/2020.
+//  Copyright © 2020 CreativeWorks Systems, Inc. All rights reserved.
+//
+
+import AppKit
+
+class MainViewController: NSViewController
+{
+    
+}
